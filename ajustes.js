71ffce05f7262a4c2761b4114fdc16c9e887a40e
CarlosCastroFernandez <bubachico@gmail.com>
2023-05-27 18:23:52 +0200

    d

--- ajustes.js
+++ ajustes.js
@@ -1,38 +1,14 @@
-
-
-function colorEnPaginas(){
-    
-    var modo=window.localStorage.getItem('--modoGuardado');
-    if(!modo[modo=document.documentElement.style.getPropertyValue('--modoGuardado')]){
-       
-    }
-    return modo;
-}
-
-function color(){
-    var color=document.getElementsByName('colorElegido')[0];
-    var colorElegido=color.value;
-    localStorage.setItem('colorElegido',colorElegido);
-    document.documentElement.style.setProperty('--colorNormal',colorElegido);
-
-}
-
-
 function aplicarCambios() {
-    
-   
-    //console.log('color elegido ' + color.value);
-    var colorElegido=localStorage.getItem('colorElegido');
-    document.documentElement.style.setProperty('--colorNormal',colorElegido);
+    // mostramos en consola los valores recogidos en la pagina
+    var color = document.getElementsByName('colorElegido')[0];
+    console.log('color elegido ' + color.value);
 
     var tam = document.getElementsByName('tamFuente')[0];
     var tamelegido = tam[tam.selectedIndex].value;
 
-
+    // aplicamos el color y el tamaño a todo el documento
     document.documentElement.style.fontSize = parseFloat(tamelegido) + "rem";
+    document.documentElement.style.setProperty("--colorNormal", color.value);
     document.getElementById('mensaje').innerHTML = "APLICADO";
 
-
 }
-
-aplicarCambios();
